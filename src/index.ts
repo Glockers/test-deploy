c6d5c57b63e8e01ed696abde913be5f33259ae59
Glockers <megamaxchanel@gmail.com>
2023-06-14 20:33:15 +0300

    we

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
 import express, { Application } from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
-import dbInit from './db/init';
 import routes from './api/routes';
-import createLogger from './api/utils/logger';
-import passport from 'passport';
-import { accessJWTStrategy } from './config/passport.config';
-import { authenticateAccess } from './api/middleware/authenticateAccess.middleware';
-import cors from 'cors';
-
-const logger = createLogger(__filename);
-dbInit();
 
 const app: Application = express();
 const PORT = process.env.API_PORT || 5000;
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-
-passport.use('access', accessJWTStrategy);
-
-app.get('/protected', authenticateAccess, (req: any, res: any) => {
-  res.json({ message: 'Защищенный маршрут' });
-});
 
 app.get('/', (_req: any, res: any) => {
   return res.send('Express Typescript on Vercel');
@@ -32,5 +15,5 @@ app.get('/', (_req: any, res: any) => {
 app.use('/api/v1', routes);
 
 app.listen(PORT, () => {
-  logger.info(`[server]: Server is running at ${process.env.API_HOST}:${PORT}`);
+  console.log(`[server]: Server is running at ${process.env.API_HOST}:${PORT}`);
 });
