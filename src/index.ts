74cab37d14824385dbc2724e7a56f4169a965e01
Glockers <megamaxchanel@gmail.com>
2023-06-14 21:48:06 +0300

    asdqwdsas

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import express, { Application } from 'express';
+import express, { Application, Response } from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
-import routes from './api/routes';
 
 const app: Application = express();
 const PORT = process.env.API_PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (_req: any, res: any) => {
-  return res.send('Express Typescript on Vercel');
+app.get('/', (_req: any, res: Response) => {
+  return res.json('Express Typescript on Vercel' + process.env.API_PORT);
 });
 
-app.use('/api/v1', routes);
+// app.use('/api/v1', routes);
 
 try {
   app.listen(PORT, () => {
