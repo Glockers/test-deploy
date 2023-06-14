aa94d69c8b78e70ded1a6085e2f50411bd8f2919
Glockers <megamaxchanel@gmail.com>
2023-06-14 20:53:10 +0300

    ghfgh

--- dist/api/routes/index.js
+++ dist/api/routes/index.js
@@ -1,13 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const meetup_router_1 = __importDefault(require("./meetup.router"));
 // import authRouter from './auth.router';
 const router = (0, express_1.Router)();
-router.use('/meetup', meetup_router_1.default);
 // router.use(authRouter);
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import meetupRouter from './meetup.router';
 // import authRouter from './auth.router';
 
 const router = Router();
 
-router.use('/meetup', meetupRouter);
 // router.use(authRouter);
 
 export default router;
