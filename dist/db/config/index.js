9ca13d8575b1dc3da79745a12086707d28b059da
Maxim <48559612+Glockers@users.noreply.github.com>
2023-07-03 19:26:51 +0300

    Delete dist directory

--- dist/db/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelizeConnection = void 0;
-var db_config_1 = require("./db.config");
-Object.defineProperty(exports, "sequelizeConnection", { enumerable: true, get: function () { return db_config_1.sequelizeConnection; } });
-//# sourceMappingURL=index.js.map
